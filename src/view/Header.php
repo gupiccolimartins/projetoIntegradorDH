8de66f82e74b7bdfc20d70df9312f92b6e2a1f81
Jorge Mercado <dotch3@gmail.com>
2020-04-29 18:51:26 -0300

    logo changed

--- src/view/Header.php
+++ src/view/Header.php
@@ -5,12 +5,14 @@
     <!-- <a href="#">
         <img src="./img/header.jpg" alt="feed header" , title="feed header" />
     </a> -->
+
+
     <div>
         <a href="#">
-            <img src="./img/logo.png" alt="logo" , title="logo" class="logo" />
+            <img src="./img/logo-2.png" width="90" height="80" alt="logo" , title="logo" class="logo" />
         </a>
-
         <nav>
+
             <ul>
                 <li><a href="../index.php">Home</a></li>
                 <li><a href="./Feed.php">Feed</a></li>
@@ -19,4 +21,5 @@
             </ul>
         </nav>
     </div>
+
 </section>
