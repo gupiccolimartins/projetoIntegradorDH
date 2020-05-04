47e97d87ec82664132dd14bc747da521d704bdf6
Jorge Mercado <dotch3@gmail.com>
2020-05-04 13:57:45 -0300

    3 opcoes added to the home page

--- src/index.php
+++ src/index.php
@@ -24,6 +24,8 @@
             <ul>
                 <li><a href="../index.php">Home</a></li>
                 <li><a href="./view/Feed.php">Feed</a></li>
+                <li><a href="./view/FeedWithGrid.php">Feed com Grids</a></li>
+                <li><a href="./view/FeedWithFlex.php">Feed com Flex</a></li>
                 <li><a href="#">Quem Somos</a></li>
                 <li><a href="#">Contato</a></li>
                 <li><a href="./view/User_View.php">Outros</a></li>
