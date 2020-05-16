dcb5be8cb276e2cf2647f4624eb60daa067bbd77
gupiccolimartins <gpiccolimartins@gmail.com>
2020-05-16 20:45:39 -0300

    alterações

--- public/index.php
+++ public/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!doctype html>
 <html lang="en">
 
@@ -6,11 +5,9 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../src/view/css/estilo_feed.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/deb957d17d.js" crossorigin="anonymous"></script>
 
 
@@ -19,7 +16,7 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #A60356;">
+    <nav class="navbar navbar-expand-sm navbar-light" style="background-color: rgba(196, 196, 196, 0.25);">
         <a class="navbar-left" href="/">
 
             <img src="../src/img/logo.png" alt="Logo" style="width: 50px; height: 50px;">
@@ -40,65 +37,66 @@
         </div>
     </nav>
 
-    <div class="container">
-        <div class="row profile">
-            <div class="col-md-3">
-                <div class="profile-sidebar">
-                    <div class="profile-userpic">
-                        <a href="#">
-                            <img src="../src/img/perfil.png" alt="perfil" , title="perfil" class="perfil" />
-                        </a>
 
-                    </div>
-                    <h1>Usuario Nome</h1>
-                    <p>Lorem Ipsum</p>
-
-                    <ul>
-                        <li>
-                            <h4>
-                                <i class="fas fa-user-circle"></i>
-                                Marcelo souza
-                            </h4>
-                        </li>
-                        <li>
-                            <h4>
-                                <i class="far fa-envelope"></i>
-                                f_souza@gmail.com
-                            </h4>
-                        </li>
-                        <li>
-                            <h4>
-                                <i class="far fa-building"></i>
-                                Apto 12 Bloco20
-                            </h4>
-                        </li>
-                        <li>
-                            <h4>
-                                <i class="fas fa-mobile-alt"></i>
-                                (XX)XXXXX-XXXX
-                            </h4>
-                        </li>
-                        <li>
-                            <h4>
-                                <i class="fas fa-users"></i>
-                                Meus vizinhos
-                            </h4>
-                        </li>
-                        <li>
-                            <h4>
-                                <i class="fas fa-users-cog"></i>
-                                Configurações
-                            </h4>
-                        </li>
-                        <li>
-                            <h4>
-                                <i class="fas fa-sign-out-alt"></i>
-                                Sair
-                            </h4>
-                        </li>
-                    </ul>
-                </div>
+    <div class="container-fluid">
+        <div class="col-md-3 user_data">
+            <h1>Usuario Nome</h1>
+            <div class="profile-userpic">
+                <a href="#">
+                    <img src="../src/img/perfil.png" alt="perfil" , title="perfil" class="perfil" />
+                </a>
             </div>
+            <ul style="list-style-type: none;">
+                <li>
+                    <h4>
+                        <i class="fas fa-user-circle"></i>
+                        Marcelo souza
+                    </h4>
+                </li>
+                <li>
+                    <h4>
+                        <i class="far fa-envelope"></i>
+                        f_souza@gmail.com
+                    </h4>
+                </li>
+                <li>
+                    <h4>
+                        <i class="far fa-building"></i>
+                        Apto 12 Bloco20
+                    </h4>
+                </li>
+                <li>
+                    <h4>
+                        <i class="fas fa-mobile-alt"></i>
+                        (XX)XXXXX-XXXX
+                    </h4>
+                </li>
+                <li>
+                    <a class="links_painel_esquerdo" href="#">
+                        <h4>
+                            <i class="fas fa-users"></i>
+                            Meus vizinhos
+                        </h4>
+                    </a>
+
+                </li>
+                <li>
+                    <a class="links_painel_esquerdo" href="#">
+                        <h4>
+                            <i class="fas fa-users-cog"></i>
+                            Configurações
+                        </h4>
+                    </a>
+                </li>
+                <li>
+                    <a class="links_painel_esquerdo" href="#">
+                        <h4>
+                            <i class="fas fa-sign-out-alt"></i>
+                            Sair
+                        </h4>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 
@@ -106,20 +104,9 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
-=======
-<?php
-require('Home.php');
-?>
->>>>>>> homologacao
