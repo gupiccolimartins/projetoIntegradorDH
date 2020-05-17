8adb16b7b70a38eaca470007af7bc28b9b9c8737
Cintia <cintiaog@hotmail.com>
2020-05-16 22:54:56 -0300

    ajuste layout user data, criacao de arquivo css proprio

--- src/view/UserData.php
+++ src/view/UserData.php
@@ -4,6 +4,7 @@ Asi funciona como exemplo de ter todos os estilos em um solo arquivo CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
     integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<link rel="stylesheet" href="./css/estilo_userData.css">
 
 <section class="user_data">
     <!-- Obtendo o nome do usuario dinamicamente segundo a pagina -->
@@ -34,23 +35,24 @@ Asi funciona como exemplo de ter todos os estilos em um solo arquivo CSS -->
     <a href="#">
         <img src="./img/avatar/<?php echo ("$usuario.png")?>" alt=" perfil" , title="perfil" class="perfil" />
     </a>
+
     <ul class="fa-ul">
-        <li><span class="fa-li"><i class="fas fa-user" style="font-size:20px ; color: #4682B4"></i>
+        <li><span class="fa-li"><i class="fas fa-user" style="font-size:20px ; color: #000000"></i>
                 </span<?php echo $nomeUsuario?>< /li>
         <li><span class="fa-li"><i class="fas fa-envelope"
-                    style="font-size:20px ; color: #4682B4"></i></span><?php echo $emailUSuario?></li>
+                    style="font-size:20px ; color: #000000"></i></span><?php echo $emailUSuario?></li>
         <li><span class="fa-li"><i class="fas fa-building"
-                    style="font-size:20px ; color: #4682B4"></i></span><?php echo $aptoUsuario?></li>
-        <li><span class="fa-li"><i class="fas fa-check-square" style="font-size:20px ; color: #4682B4"></i></span>Seguir
-            - Mensagem</li>
-        <li><span class="fa-li"><i class="fas fa-users" style="font-size:20px ; color: #4682B4"></i></span>3 Vizinhos
+                    style="font-size:20px ; color: #000000"></i></span><?php echo $aptoUsuario?></li>
+        <li><span class="fa-li"><i class="fas fa-check-square" style="font-size:20px ; color: #000000"></i></span> Mensagem</li>
+        <!-- <li><span class="fa-li"><i class="fas fa-users" style="font-size:20px ; color: #000000"></i></span>3 Vizinhos -->
         </li>
-        <li><span class="fa-li"><i class="fas fa-people-carry" style="font-size:20px ; color: #4682B4"></i></span>3
-            Emprestimos</li>
+        <!-- <li><span class="fa-li"><i class="fas fa-people-carry" style="font-size:20px ; color: #000000"></i></span>3 -->
+            <!-- Emprestimos</li> -->
         <li><span class="fa-li"><i class="fas fa-thumbs-up"
-                    style="font-size:20px ; color: #4682B4"></i></span>Recomendeme</li>
-        <li><span class="fa-li"><i class="fas fa-heart" style="font-size:20px ; color: #4682B4"></i></span>0
+                    style="font-size:20px ; color: #000000"></i></span>Recomendeme</li>
+        <li><span class="fa-li"><i class="fas fa-heart" style="font-size:20px ; color: #000000"></i></span>0
             Recomendaçoes</li>
     </ul>
     </ul>
+
 </section>
