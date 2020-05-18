8ec1e6d96db2a390b7031ddf7c40814ce0fa453c
gupiccolimartins <gpiccolimartins@gmail.com>
2020-05-18 14:48:46 -0300

    arrumando formulario

--- public/cadastro.php
+++ public/cadastro.php
@@ -25,56 +25,67 @@
 
     </main>
 
-    <h2> Cadastro <h2> <br />
-            <div class="fundo_img">Sua Foto</div><br>
-            <div class="img_upload">
-                <input type="file" name="foto" placeholder="Sua foto">
-                <hr /> <br />
+
+
+    <div class="container cadastro">
+
+        <div class="row no-gutters">
+            <div class="col-3 fundo_img">
+                <h2>Sua Foto</h2>
             </div>
-            <div class="cadastro">
-                <div>
-                    <form id="frmPost" action="">
-                        <form method="get" action="envio_dados.php">
-                            <input type="text" name="nome" placeholder="Nome Completo">
-                            <hr /> <br />
-                            <input type="text" name="cpf" placeholder="CPF" onkeydown="javascript: fMasc( this, mCPF );">
-                            <hr /> <br />
-                            <input type="text" name="telefone" placeholder="Telefone" onkeydown="javascript: fMasc( this, mTel );">
-                            <hr /> <br />
+            <div class="img_upload">
+                <input type="file" name="foto">
+            </div>
+            
+            <div class="col-1"></div>
+    
+            <div class="col-4 text-center">
+                <h2> Cadastro </h2>
+                <form id="frmPost" action="">
+                    <form method="get" action="envio_dados.php">
+                        <input type="text" name="nome" placeholder="Nome Completo">
+                        <hr /> <br />
+                        <input type="text" name="cpf" placeholder="CPF" onkeydown="javascript: fMasc( this, mCPF );">
+                        <hr /> <br />
+                        <input type="text" name="telefone" placeholder="Telefone" onkeydown="javascript: fMasc( this, mTel );">
+                        <hr /> <br />
 
-                            <input type="e-mail" name="email" placeholder="Seu e-mail">
-                            <hr /> <br />
+                        <input type="e-mail" name="email" placeholder="Seu e-mail">
+                        <hr /> <br />
 
 
 
-                            <div>
-                                <div class="campo1">
-                                    <input type="text" name="ap" placeholder="Apto">
-                                    <input type="text" name="bloco" placeholder="Bloco">
-                                    <hr /> <br />
-                                </div>
-                                <input type="text" name="interfone" placeholder="Interfone">
-                                <hr /><br />
-
+                        <div>
+                            <div class="campo1">
+                                <input type="text" name="ap" placeholder="Apto">
+                                <input type="text" name="bloco" placeholder="Bloco">
+                                <hr /> <br />
                             </div>
+                            <input type="text" name="interfone" placeholder="Interfone">
+                            <hr /><br />
 
-                            <div class="button_cadastro">
+                        </div>
 
-                                <input type="submit" name="Cadastrar" value="Cadastrar">
-                            </div>
+                        <div class="button_cadastro">
 
-                        </form>
-                </div>
+                            <input type="submit" name="Cadastrar" value="Cadastrar">
+                        </div>
 
-                </main>
-                <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-                </script>
-                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-                </script>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-                </script>
+                    </form>
+            </div>
 
-                
+            </main>
+            <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+            </script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+            </script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+            </script>
+            <div class="col-4"></div>
+        </div>
+    </div>
+
+
 
 </body>
 
